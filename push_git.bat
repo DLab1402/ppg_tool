22d4f99bbf04642f890d958f02afdcdde474dadf
DLab1402 <phankhanhduy.tg.vl@gmail.com>
2025-09-30 18:32:54 +0700

    30/9/25

--- push_git.bat
+++ push_git.bat
@@ -2,11 +2,11 @@
 REM Go to project directory (optional)
 cd /d %~dp0
 
-REM Activate the virtual environment
-call myenv\Scripts\activate.bat
+
 
 REM Run your Python script inside the venv
-python main.py
+git add .
+git commit -m "22/9//25"
 
 REM Pause so you can see any errors before window closes
 pause 
